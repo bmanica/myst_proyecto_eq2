b22a44bdaaf9687e3c8c9223387845040e1001db
Bryan Juárez <if722176@iteso.mx>
2022-06-30 18:47:39 -0500

    Add link

--- data.py
+++ data.py
@@ -5,7 +5,7 @@
 # -- script: data.py : python script for data collection                                                 -- #
 # -- author: YOUR GITHUB USER NAME                                                                       -- #
 # -- license: THE LICENSE TYPE AS STATED IN THE REPOSITORY                                               -- #
-# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- repository: https://github.com/bmanica/myst_proyecto_eq2                                            -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 
